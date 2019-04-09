569cbed92396c22939fffa2a0ee293ea4a36c563
itssomething <vdmanh107@gmail.com>
2019-04-09 09:12:41 +0700

    make a test api

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 
 my_awesome_app = Flask(__name__)
 
@@ -7,6 +8,13 @@ my_awesome_app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@my_awesome_app.route('/api/diabetes', methods=['POST'])
+def diabetes_api():
+	req_data = request.get_json()
+
+	key = req_data["key"]
+
+	return key
 
 if __name__ == '__main__':
     my_awesome_app.run()
